c65199e6572e598b535ce9e2f1e54fa85af8dc24
krishnadobhal <107910364+krishnadobhal@users.noreply.github.com>
2023-11-01 16:32:48 +0530

    Added new logo+ centered info

--- controller/Control.js
+++ controller/Control.js
@@ -1,53 +1,86 @@
 const Utils = require('./Util');
-const https=require("https")
-const express=require("express");
-const app=express();
-app.set('view engine', 'ejs')   
+const https = require("https")
+const express = require("express");
+const app = express();
+app.set('view engine', 'ejs')
 
 
 
 
-const mainRoute=(req,res)=>{
-    res.render("index");
-}
-
-
-
-const Search=(req,res)=>{
-    const cityName=req.body.cityName;
-    const apikey="24be5d3fa2d486a7e6e67ab9f3624567";
-    const url="https://api.openweathermap.org/data/2.5/forecast?q="+cityName+"&appid="+apikey;
+const mainRoute = (req, res) => {
+    const cityName = "Delhi";
+    const apikey = "24be5d3fa2d486a7e6e67ab9f3624567";
+    const url = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apikey;
 
     //API call
-    https.get(url,function(response){
-        if(response.statusCode=="404"){
+    https.get(url, function (response) {
+        if (response.statusCode == "404") {
             res.render("fail");
         }
-        console.log(url);
+        
         let resultData = '';
         response.on('data', data => resultData += data);
-        
-        response.on("end",()=>{
-            const weatherData=JSON.parse(resultData);
-            const temp=Utils.KtoCelsius(weatherData.list[0].main.temp);
-            const type=weatherData.list[0].weather[0].description;
-            const img=weatherData.list[0].weather[0].icon;
-            const humidity=weatherData.list[0].main.humidity;
-            const windSpeed=weatherData.list[0].wind.speed;
-            const windSpeedDirection=Utils.windDegreeToDirection(weatherData.list[0].wind.speed);
-            const pressure=weatherData.list[0].main.pressure;
-            const min=Utils.KtoCelsius(weatherData.list[0].main.temp_min);
-            const max=Utils.KtoCelsius(weatherData.list[0].main.temp_max);
+
+        response.on("end", () => {
+            const weatherData = JSON.parse(resultData);
+            const temp = Utils.KtoCelsius(weatherData.list[0].main.temp);
+            const type = weatherData.list[0].weather[0].description;
+            const img = Utils.SelectIcon(weatherData.list[0].weather[0].icon);
+            const humidity = weatherData.list[0].main.humidity;
+            const windSpeed = weatherData.list[0].wind.speed;
+            const windSpeedDirection = Utils.windDegreeToDirection(weatherData.list[0].wind.speed);
+            const pressure = weatherData.list[0].main.pressure;
+            const min = Utils.KtoCelsius(weatherData.list[0].main.temp_min);
+            const max = Utils.KtoCelsius(weatherData.list[0].main.temp_max);
 
             const timestamp = weatherData.list[0].dt;
             const formatDate = new Date(timestamp * 1000); // Convert Unix timestamp to Date
             const date = Utils.formatDate(formatDate);
-            
-            const imgURL="https://openweathermap.org/img/wn/"+img+"@4x.png"
-            res.render("target",{Date:date,City:cityName,Temp:temp,Min:min,Max:max,Type:type,Humidity:humidity,WindSpeed:windSpeed,WindSpeedDirection:windSpeedDirection,Pressure:pressure,img:imgURL});
+
+
+            console.log(img);
+            res.render("index", { Date: date, City: cityName, Temp: temp, Min: min, Max: max, Type: type, Humidity: humidity, WindSpeed: windSpeed, WindSpeedDirection: windSpeedDirection, Pressure: pressure, img: img });
         })
     })
 }
-module.exports={
-    mainRoute,Search
+
+
+
+const Search = (req, res) => {
+    const cityName = req.body.cityName;
+    const apikey = "24be5d3fa2d486a7e6e67ab9f3624567";
+    const url = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apikey;
+
+    //API call
+    https.get(url, function (response) {
+        if (response.statusCode == "404") {
+            res.render("fail");
+        }
+        else {
+            let resultData = '';
+            response.on('data', data => resultData += data);
+
+            response.on("end", () => {
+                const weatherData = JSON.parse(resultData);
+                const temp = Utils.KtoCelsius(weatherData.list[0].main.temp);
+                const type = weatherData.list[0].weather[0].description;
+                const img = Utils.SelectIcon(weatherData.list[0].weather[0].icon);
+                const humidity = weatherData.list[0].main.humidity;
+                const windSpeed = weatherData.list[0].wind.speed;
+                const windSpeedDirection = Utils.windDegreeToDirection(weatherData.list[0].wind.speed);
+                const pressure = weatherData.list[0].main.pressure;
+                const min = Utils.KtoCelsius(weatherData.list[0].main.temp_min);
+                const max = Utils.KtoCelsius(weatherData.list[0].main.temp_max);
+
+                const timestamp = weatherData.list[0].dt;
+                const formatDate = new Date(timestamp * 1000); // Convert Unix timestamp to Date
+                const date = Utils.formatDate(formatDate);
+
+                res.render("index", { Date: date, City: cityName, Temp: temp, Min: min, Max: max, Type: type, Humidity: humidity, WindSpeed: windSpeed, WindSpeedDirection: windSpeedDirection, Pressure: pressure, img: img });
+            })
+        }
+    })
+}
+module.exports = {
+    mainRoute, Search
 }
--- controller/Util.js
+++ controller/Util.js
@@ -25,7 +25,55 @@ function KtoCelsius(Ktemp){
     const temp=Decimaltemp.toFixed(1);
     return temp;
 }
+
+function SelectIcon(icon){
+    if(icon=="01d"){
+        return "/Icons/clear-day.svg";
+    }
+    else if(icon=="01n"){
+        return "/Icons/clear-night.svg";
+    }
+    else if(icon=="01n"){
+        return "/Icons/clear-night.svg";
+    }
+    else if(icon=="02d"){
+        return "/Icons/few cloud-night.svg";
+    }
+    else if(icon=="02n"){
+        return "/Icons/few cloud-night.svg";
+    }
+    else if(icon=="03d" || icon=="03n"){
+        return "/Icons/scattered clouds.svg";
+    }
+    else if(icon=="04d" || icon=="04n"){
+        return "/Icons/broken clouds.svg";
+    }
+    else if(icon=="09d" || icon=="09n"){
+        return "/Icons/shower rain.svg";
+    }
+    else if(icon=="10d"){
+        return "/Icons/rain-day.svg";
+    }
+    else if(icon=="10n"){
+        return "/Icons/rain-night.svg";
+    }
+    else if(icon=="11d"){
+        return "/Icons/thunderstorm-day.svg";
+    }
+    else if(icon=="11n"){
+        return "/Icons/thunderstorm-night.svg";
+    }
+    else if(icon=="13d" || icon=="13n"){
+        return "/Icons/Snow.svg";
+    }
+    else if(icon=="50d" || icon=="50n"){
+        return "/Icons/mist.svg";
+    }
+
+
+}
+
 // Export the functions for use in other files
 module.exports = {
-    formatDate,windDegreeToDirection,KtoCelsius
+    formatDate,windDegreeToDirection,KtoCelsius,SelectIcon
 };
