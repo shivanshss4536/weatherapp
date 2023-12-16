6730ec5a338a28f93f9208c4efb2fafc2eadd177
shivanshss4536 <148360608+shivanshss4536@users.noreply.github.com>
2023-12-16 13:12:20 +0000

    cloud icon name changed

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs')
 //Routes
 app.use('/',Route);
 
-const PORT=process.env.PORT || 80
+const PORT=process.env.PORT || 3000;
 app.listen(PORT,()=>{
     console.log("listening");
 })
